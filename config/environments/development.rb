fc01a0a6bf27bb9cf957b94af18538ee982e639c
guy-roberts <groberts2001@hotmail.com>
2021-02-26 15:45:24 +0000

    Urgh

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,5 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "eyfs-dev.london.cloudapps.digital"
+  config.hosts << "config.hosts << "early-years-foundation-reform.herokuapp.com"
 end
